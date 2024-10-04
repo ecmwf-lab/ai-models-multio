025386fcb0e0c21848e18d76ceb04ae76cd84585
Harrison Cook <harrison.cook@ecmwf.int>
2024-10-04 11:03:01 +0000

    Update to use better templates - tmpl from eccodes

--- src/ai_models_multio/plans.py
+++ src/ai_models_multio/plans.py
@@ -7,8 +7,11 @@
 
 from __future__ import annotations
 
+import logging
 import os
+import warnings
 from pathlib import Path
+from typing import TYPE_CHECKING
 from typing import Literal
 
 from multiopython.plans import Config
@@ -18,14 +21,18 @@ from multiopython.plans import sinks
 
 PLANS = Literal["to_file", "to_fdb", "debug"]
 
-template_path = Path(__file__).parent / "templates" / "template.grib"
+if TYPE_CHECKING:
+    import numpy as np
+    from earthkit.data.core.metadata import Metadata
+
+LOG = logging.getLogger(__name__)
 
 
 class CONFIGURED_PLANS:
     """Configured plans for Multio Output"""
 
     @staticmethod
-    def to_file(output_path: os.PathLike, grid_type: str = "n320", **_) -> Config:
+    def to_file(output_path: os.PathLike, template_path: os.PathLike, grid_type: str = "n320", **_) -> Config:
         return Plan(
             actions=[
                 actions.Encode(template=str(template_path), format="grib", grid_type=grid_type),
@@ -43,7 +50,7 @@ class CONFIGURED_PLANS:
         ).to_config()
 
     @staticmethod
-    def to_fdb(output_path: os.PathLike, grid_type: str = "n320", **_) -> Config:
+    def to_fdb(output_path: os.PathLike, template_path: os.PathLike, grid_type: str = "n320", **_) -> Config:
         return Plan(
             actions=[
                 actions.Encode(template=str(template_path), format="grib", grid_type=grid_type),
@@ -60,13 +67,61 @@ class CONFIGURED_PLANS:
         ).to_config()
 
 
-def get_plan(plan: PLANS, **kwargs) -> Config:
+def get_template_path(values: np.ndarray, metadata: Metadata) -> str:
+    """Get path to the template file
+
+    Uses earthkit.data.readers.grib.output.GribCoder to determine the template file
+
+    Pulls from in order:
+        - ai_models_multio/templates
+        - $ECCODES_DIR/share/eccodes/samples
+        and fails over to the default template
+
+    Returns
+    -------
+    str
+        Path to the template file
+    """
+    from earthkit.data.readers.grib.output import GribCoder
+
+    coder = GribCoder()
+    metadata = dict(metadata)
+
+    if len(values.shape) == 1:
+        template_name = coder._gg_field(values, metadata)
+    elif len(values.shape) == 2:
+        template_name = coder._ll_field(values, metadata)
+    else:
+        warnings.warn(f"Invalid shape {values.shape} for GRIB, must be 1 or 2 dimension ", RuntimeWarning)
+        template_name = "default"
+
+    template_path = (Path(__file__).parent / "templates" / (template_name + ".tmpl")).absolute()
+
+    if not template_path.exists():
+        if "ECCODES_DIR" in os.environ:
+            template_path = (
+                Path(os.environ["ECCODES_DIR"]) / "share" / "eccodes" / "samples" / (template_name + ".tmpl")
+            )
+        else:
+            warnings.warn(f"Template {template_path} does not exist, using default template", RuntimeWarning)
+            template_path = Path(__file__).parent / "templates" / "default.tmpl"
+
+    LOG.info(f"Using template {template_path}")
+
+    return str(template_path.absolute())
+
+
+def get_plan(plan: PLANS, values: np.ndarray, metadata: Metadata, **kwargs) -> Config:
     """Get plan for Multio Output
 
     Parameters
     ----------
     plan : PLANS
         Plan ID to get
+    values : np.ndarray
+        Values to find template from
+    metadata : Metadata
+        Metadata for the values, used to determine the template
     kwargs : dict
         Additional parameters for the plan
 
@@ -75,4 +130,5 @@ def get_plan(plan: PLANS, **kwargs) -> Config:
     Config
         Multio Plan configuration
     """
-    return getattr(CONFIGURED_PLANS, plan)(**kwargs)
+    template_path = get_template_path(values, metadata)
+    return getattr(CONFIGURED_PLANS, plan)(template_path=template_path, **kwargs)
---
+++ src/ai_models_multio/templates/README.md
@@ -0,0 +1,7 @@
+# Grib Templates
+
+This is a subset of grib templates sourced from `eccodes`.
+
+Which one to use is dependent on the result of `earthkit.data.readers.grib.output.GribCoder`
+
+If not found will fail over to `default.tmpl`
